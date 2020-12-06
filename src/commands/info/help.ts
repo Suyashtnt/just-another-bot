d032d741a9ee7d82a0b977b6f4ca7e61b0c5abd6
a person <suyashtnt@gmail.com>
2020-12-06 20:56:41 +0200

    cleanup some of help.ts and add perms.ts if you are missing perms

--- src/commands/info/help.ts
+++ src/commands/info/help.ts
@@ -3,105 +3,92 @@ import {Message, MessageEmbed} from 'discord.js';
 import * as luxon from 'luxon';
 
 export default class HelpCommand extends Command {
-	constructor() {
-		super('help', {
-			aliases: ['help', 'h'],
-			args: [
-				{
-					id: 'commandName',
-				},
-			],
-			description: {
-				text: 'Displays this message',
-				usage: `help [commandname?]`,
-			},
-			category: 'info',
-			cooldown: 1000 * 60,
-		});
-	}
+    constructor() {
+        super('help', {
+            aliases: ['help', 'h'],
+            args: [
+                {
+                    id: 'commandName',
+                },
+            ],
+            description: {
+                text: 'Displays this message',
+                usage: `help [commandname?]`,
+            },
+            category: 'info',
+            cooldown: 1000 * 60,
+        });
+    }
 
-	async exec(message: Message, args: any) {
-		message.channel.send(`Helping...`).then((msg) => {
-			if (!args.commandName) {
-				const _ = new MessageEmbed()
-					.setTitle("Here's your help!")
-					.setColor(`#00FF00`)
-					.setFooter('if a argument has a ? attached to it its optional');
-				this.handler.categories.each((category) => {
-					_.addField(
-						category.id,
-						`${category
-							.map(
-								(command) =>
-									`\`${this.client.guildSettings.get(
-										message.guild.id,
-										'prefix',
-										'r!',
-									)}${command.description.usage}\` - ${
-										command.description.text
-									}\n\n`,
-							)
-							.join('')}`,
-						true,
-					);
-				});
-				message.channel.send(_);
-				msg.delete();
-			} else {
-				const cmd = this.client.commandHandler.findCommand(args.commandName);
-				if (!cmd) return msg.channel.send('cannot find that command');
-				console.log(cmd.userPermissions);
+    async exec(message: Message, args: any) {
+        message.channel.send(`Helping...`).then((msg) => {
+            if (!args.commandName) {
+                const _ = new MessageEmbed()
+                    .setTitle("Here's your help!")
+                    .setColor(`#00FF00`)
+                    .setFooter('if a argument has a ? attached to it its optional');
+                this.handler.categories.each((category) => {
+                    _.addField(
+                        category.id,
+                        `${category
+                            .map(
+                                (command) =>
+                                    `\`${this.client.guildSettings.get(
+                                        message.guild.id,
+                                        'prefix',
+                                        'r!',
+                                    )}${command.description.usage}\` - ${
+                                        command.description.text
+                                    }\n\n`,
+                            )
+                            .join('')}`,
+                        true,
+                    );
+                });
+                message.channel.send(_);
+                msg.delete();
+            } else {
+                const cmd = this.client.commandHandler.findCommand(args.commandName);
+                if (!cmd) return msg.channel.send('cannot find that command');
+                console.log(cmd.userPermissions);
 
-				let userPerms = cmd.userPermissions ? cmd.userPermissions : [];
-				if (userPerms) {
-					//@ts-ignore
-					userPerms.forEach(function (item: string, index: number) {
-						const caseElement =
-							item[0].toUpperCase() + item.slice(1).toLowerCase();
-						console.log(caseElement);
+                // @ts-ignore
+                let userPerms: string[] = cmd.userPermissions ? cmd.userPermissions instanceof Array ? cmd.userPermissions : [cmd.userPermissions] : [];
 
-						userPerms[index] = caseElement;
-					});
-				}
-				//@ts-ignore
-				const userPermsString = userPerms.join(', ');
-				let userPermsStringSpaced = userPermsString.replace('_', ' ');
+                const userPermsString = userPerms.map((item) => {
+                    return item[0].toUpperCase() + item.slice(1).toLowerCase();
+                }).join(", ").replace("_", " ");
 
-				let clientPerms = cmd.clientPermissions ? cmd.clientPermissions : [];
-				//@ts-ignore
-				clientPerms.forEach(function (item: string, index: number) {
-					const caseElementelement =
-						item[0].toUpperCase() + item.slice(1).toLowerCase();
-					console.log(caseElementelement);
-					clientPerms[index] = caseElementelement;
-				});
-				//@ts-ignore
-				const clientPermsString = clientPerms.join(', ');
-				let clientPermsStringSpaced = clientPermsString.replace('_', ' ');
+                // @ts-ignore
+                let clientPerms: string[] = cmd.clientPermissions ? cmd.clientPermissions instanceof Array ? cmd.clientPermissions : [cmd.clientPermissions] : [];
 
-				const _ = new MessageEmbed()
-					.setTitle(`\`${cmd.aliases.join(', ')}\` - ${cmd.category}`)
-					.addField('Usage: ', `**${cmd.description.usage}**`)
-					.addField(
-						'Needed User Permissions:',
-						`${userPermsStringSpaced ? userPermsStringSpaced : 'none'}`,
-					)
-					.addField(
-						'Needed Bot Permissions:',
-						`${clientPermsStringSpaced ? clientPermsStringSpaced : 'none'}`,
-					)
-					.addField(
-						'cooldown',
-						cmd.cooldown
-							? luxon.Duration.fromMillis(cmd.cooldown).toFormat(
-									"'hours: ' hh ' | minutes: ' mm ' | seconds: ' ss",
-							  )
-							: 'none',
-					)
-					.setFooter('if a argument has a ? attached to it its optional');
-				message.channel.send(_);
-				msg.delete();
-			}
-		});
-	}
+                const clientPermsString = clientPerms.map((item) => {
+                    return item[0].toUpperCase() + item.slice(1).toLowerCase();
+                }).join(", ").replace("_", " ");
+
+                const _ = new MessageEmbed()
+                    .setTitle(`\`${cmd.aliases.join(', ')}\` - ${cmd.category}`)
+                    .addField('Usage: ', `**${cmd.description.usage}**`)
+                    .addField(
+                        'Needed User Permissions:',
+                        `${userPermsString ? userPermsString : 'none'}`,
+                    )
+                    .addField(
+                        'Needed Bot Permissions:',
+                        `${clientPermsString ? clientPermsString : 'none'}`,
+                    )
+                    .addField(
+                        'cooldown',
+                        cmd.cooldown
+                            ? luxon.Duration.fromMillis(cmd.cooldown).toFormat(
+                            "'hours: ' hh ' | minutes: ' mm ' | seconds: ' ss",
+                            )
+                            : 'none',
+                    )
+                    .setFooter('if a argument has a ? attached to it its optional');
+                message.channel.send(_);
+                msg.delete();
+            }
+        });
+    }
 }
---
+++ src/listeners/perms.ts
@@ -0,0 +1,20 @@
+import {Command, Listener} from 'discord-akairo';
+import {Message} from 'discord.js';
+
+export default class Perms extends Listener {
+    constructor() {
+        super('perms', {
+            emitter: 'commandHandler',
+            event: 'missingPermissions',
+            category: 'command',
+        });
+    }
+
+    async exec(message: Message, command: Command, type: 'client' | 'user', missing: string[]) {
+        const casedPerms = missing.map((item: string) => {
+            return item[0].toUpperCase() + item.slice(1).toLowerCase();
+        }).join(", ").replace("_", " ");
+
+        await message.channel.send(`${type === "client" ? "I'm" : "You're"} missing the following perms needed to run \`${command.aliases[0]}\`: ${casedPerms}`)
+    }
+}
