7adf15516db88d910c76c61195fd607a66eed203
a person <suyashtnt@gmail.com>
2020-12-07 13:09:28 +0200

    feat(HelpCommand): add a reaction to delete the message chore(Deps): update deps feat(Yarn): add ts plugin for types adding

--- src/commands/info/help.ts
+++ src/commands/info/help.ts
@@ -22,72 +22,87 @@ export default class HelpCommand extends Command {
 
 	async exec(message: Message, args: { commandName: string }) {
 
-		message.channel.send('Helping...').then(async (msg) => {
-			if (!args.commandName) {
-				const embed = new MessageEmbed()
-					.setTitle('Here\'s your help!')
-					.setColor('#00FF00')
-					.setFooter('if a argument has a ? attached to it its optional. If a command name has a strikethrough format you dont have enough perms to run it.');
+		const msg = await message.channel.send('Helping...');
 
-				const prefix = await this.client.guildSettings.get(
-					message.guild.id,
-					'prefix',
-					'r!',
-				);
+		if (!args.commandName) {
+			const embed = new MessageEmbed()
+				.setTitle('Here\'s your help!')
+				.setColor('#00FF00')
+				.setFooter('if a argument has a ? attached to it its optional. If a command name has a strikethrough format you dont have enough perms to run it.');
 
-				this.handler.categories.map((category) =>
-					embed.addField(
-						category.id,
-						`${category
-							.map(
-								(command) => {
-									const hasPerms = !message.member.hasPermission(<PermissionResolvable[]>command.userPermissions);
-									return `${hasPerms ? '~~' : ''}${prefix}${command.description.usage}${hasPerms ? '~~' : ''} | ${command.description.text}\n---------\n`;
-								},
-							)
-							.join('')}`,
-						true,
-					),
-				);
+			const prefix = await this.client.guildSettings.get(
+				message.guild.id,
+				'prefix',
+				'r!',
+			);
 
-				await message.channel.send(embed);
-				await msg.delete();
-			} else {
-				const cmd = this.client.commandHandler.findCommand(args.commandName);
-				if (!cmd) return msg.channel.send('I could not find that command');
+			this.handler.categories.map((category) =>
+				embed.addField(
+					category.id,
+					`${category
+						.map(
+							(command) => {
+								const hasPerms = !message.member.hasPermission(<PermissionResolvable[]>command.userPermissions);
+								return `${hasPerms ? '~~' : ''}${prefix}${command.description.usage}${hasPerms ? '~~' : ''} | ${command.description.text}\n---------\n`;
+							},
+						)
+						.join('')}`,
+					true,
+				),
+			);
 
-				const userPerms = (<PermissionResolvable[]>cmd.userPermissions) ? (<PermissionResolvable[]>cmd.userPermissions).map((val) => val.toString()) : null;
-				const userPermsString = userPerms ? userPerms
-					.map((item) => {
-						return item[0].toUpperCase() + item.slice(1).toLowerCase();
-					})
-					.join(', ')
-					.replace('_', ' ') : null;
+			const sent = await message.channel.send(embed);
+			await msg.delete();
 
-				const clientPerms = (<PermissionResolvable[]>cmd.clientPermissions) ? (<PermissionResolvable[]>cmd.clientPermissions).map((val) => val.toString()) : null;
-				const clientPermsString = clientPerms ? clientPerms
-					.map((item) => item[0].toUpperCase() + item.slice(1).toLowerCase())
-					.join(', ')
-					.replace('_', ' ') : null;
+			try {
+				await sent.react('🗑');
+				const waited = await sent.awaitReactions((reaction, user) => {
+					return ['🗑'].includes(reaction.emoji.name) && user.id === message.author.id;
+				}, { max: 1, time: 60000, errors: ['time'] });
 
-				const embed = new MessageEmbed()
-					.setTitle(`\`${cmd.aliases[0]}\` from \`${cmd.category}\``)
-					.setDescription(cmd.description.text)
-					.addField('Usage', cmd.description.usage)
-					.addField('Aliases', `\`${cmd.aliases.join('` | `')}\``)
-					.addField(
-						'Needed User Permissions',
-						userPermsString ? userPermsString : 'none',
-					)
-					.addField(
-						'Needed Bot Permissions',
-						`${clientPermsString ? clientPermsString : 'none'}`,
-					)
-					.addField('cooldown', cmd.cooldown ? formatMS(cmd.cooldown) : 'none')
-					.setFooter('if a argument has a ? attached to it its optional');
-				await message.channel.send(embed);
-				await msg.delete();
+				const reaction = waited.first();
+				if (reaction.emoji.name !== '🗑') return await message.channel.send('you reacted to something that isn\'t a 🗑');
+			} catch (e) {
+				console.log(e);
 			}
-		});
+
+			await sent.delete();
+			await message.channel.send('deleted help message');
+		} else {
+			const cmd = this.client.commandHandler.findCommand(args.commandName);
+			if (!cmd) return msg.channel.send('I could not find that command');
+
+			const userPerms = (<PermissionResolvable[]>cmd.userPermissions) ? (<PermissionResolvable[]>cmd.userPermissions).map((val) => val.toString()) : null;
+			const userPermsString = userPerms ? userPerms
+				.map((item) => {
+					return item[0].toUpperCase() + item.slice(1).toLowerCase();
+				})
+				.join(', ')
+				.replace('_', ' ') : null;
+
+			const clientPerms = (<PermissionResolvable[]>cmd.clientPermissions) ? (<PermissionResolvable[]>cmd.clientPermissions).map((val) => val.toString()) : null;
+			const clientPermsString = clientPerms ? clientPerms
+				.map((item) => item[0].toUpperCase() + item.slice(1).toLowerCase())
+				.join(', ')
+				.replace('_', ' ') : null;
+
+			const embed = new MessageEmbed()
+				.setTitle(`\`${cmd.aliases[0]}\` from \`${cmd.category}\``)
+				.setDescription(cmd.description.text)
+				.addField('Usage', cmd.description.usage)
+				.addField('Aliases', `\`${cmd.aliases.join('` | `')}\``)
+				.addField(
+					'Needed User Permissions',
+					userPermsString ? userPermsString : 'none',
+				)
+				.addField(
+					'Needed Bot Permissions',
+					`${clientPermsString ? clientPermsString : 'none'}`,
+				)
+				.addField('cooldown', cmd.cooldown ? formatMS(cmd.cooldown) : 'none')
+				.setFooter('if a argument has a ? attached to it its optional');
+			await message.channel.send(embed);
+			await msg.delete();
+		}
 	}
 }
--- src/config.example.ts
+++ src/config.example.ts
@@ -1,3 +1,4 @@
+// rename to config.ts
 export default {
 	token: 'bot-token',
 	mongoUrl: 'mongodb://for-version-2.4.0-or-something-like-that',
